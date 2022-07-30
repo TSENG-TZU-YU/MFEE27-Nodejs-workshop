f9fdc6463568224849c836e8a4a80a1aabceeb11
Joy <q39078273@gmail.com>
2022-07-30 11:49:03 +0800

    完成

--- sum.js
+++ sum.js
@@ -1,8 +1,15 @@
 function sum(n){
    // 1+2+3..
-   
+   let res=0;
+   for(let i=0; i<=n; i++){
+    res+=i;
+    
+   }
+
+   return res;
 }
 
-console.log(sum(1));
-console.log(sum(3));
-console.log(sum(10));
+console.log(sum(1));//1
+console.log(sum(3));//6
+console.log(sum(5));//15
+
