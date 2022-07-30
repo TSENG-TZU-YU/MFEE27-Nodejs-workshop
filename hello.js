31efca35e24f102bcde24027d277315c5daf7e06
Joy <q39078273@gmail.com>
2022-07-30 11:35:37 +0800

    題目

---
+++ hello.js
@@ -0,0 +1,4 @@
+console.log("hello nodejs")
+
+//因為 window 是瀏覽器 因此無法執行
+window.location.href = "https://www.youtube.com/"
---
+++ sum.js
@@ -0,0 +1,8 @@
+function sum(n){
+   // 1+2+3..
+   
+}
+
+console.log(sum(1));
+console.log(sum(3));
+console.log(sum(10));
