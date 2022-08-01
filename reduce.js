923be8e200df6d8747359da10d35eee2ecddd77a
Joy <q39078273@gmail.com>
2022-08-01 18:16:31 +0800

    [feat] add reduce.js

---
+++ reduce.js
@@ -0,0 +1,31 @@
+let ary = [
+  {
+    id: 1,
+    type: "A",
+    price: 100,
+  },
+  {
+    id: 2,
+    type: "B",
+    price: 200,
+  },
+  {
+    id: 3,
+    type: "A",
+    price: 150,
+  },
+];
+
+//加總價錢
+let result = ary.reduce((acc, cur) => acc + cur.price, 0);
+// console.log(result);
+
+//for-loop
+function reduce(ary) {
+    let result=0;
+  for (let i = 0; i < ary.length; i++) {
+    result+=ary[i].price;
+  }
+  return result;
+}
+console.log(reduce(ary))
